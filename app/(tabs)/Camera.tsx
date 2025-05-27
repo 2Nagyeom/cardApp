781edabebb3f2bbc8e34d8199d7c8e0ff13a616c
2Nagyeom <artest1081@gmail.com>
2025-05-27 14:51:57 +0900

    expo camera install fin

--- app/(tabs)/Camera.tsx
+++ app/(tabs)/Camera.tsx
@@ -1,35 +1,68 @@
-import { Image, SafeAreaView, StyleSheet } from 'react-native';
-
-import { HeaderBar } from '@/components/ui/HeaderBar';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import { router } from 'expo-router';
+import { CameraType, CameraView, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function TabTwoScreen() {
+    const [facing, setFacing] = useState<CameraType>('back');
+    const [permission, requestPermission] = useCameraPermissions();
+
+    if (!permission) {
+        // Camera permissions are still loading.
+        return <View />;
+    }
+
+    if (!permission.granted) {
+        // Camera permissions are not granted yet.
+        return (
+            <View style={styles.container}>
+                <Text style={styles.message}>We need your permission to show the camera</Text>
+                <Button onPress={requestPermission} title="grant permission" />
+            </View>
+        );
+    }
+
+    function toggleCameraFacing() {
+        setFacing(current => (current === 'back' ? 'front' : 'back'));
+    }
     return (
-        <SafeAreaView style={{flex : 1, backgroundColor : '#fff'}}>
-            <HeaderBar
-                title="CardLab"
-                rightButton={{
-                    child : <IconSymbol size={24} name={'magnifyingglass'} color={'#1A1A2E'} />,
-                    onPress : () => router.push('/Result')
-                }}
-                leftButton={{
-                    child : <Image source={require('@/assets/icons/cardLabIcon.png')} style={{width : 44, height :44}} />
-                }}
-            />
-        </SafeAreaView>
+        <View style={styles.container}>
+            <CameraView style={styles.camera} facing={facing}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+                        <Text style={styles.text}>Flip Camera</Text>
+                    </TouchableOpacity>
+                </View>
+            </CameraView>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
-    headerImage: {
-        color: '#808080',
-        bottom: -90,
-        left: -35,
-        position: 'absolute',
+    container: {
+        flex: 1,
+        justifyContent: 'center',
     },
-    titleContainer: {
+    message: {
+        textAlign: 'center',
+        paddingBottom: 10,
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
         flexDirection: 'row',
-        gap: 8,
+        backgroundColor: 'transparent',
+        margin: 64,
+    },
+    button: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
     },
 });
