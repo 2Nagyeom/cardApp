bb4a3ef8b1664b69d8476509340f38b9c035c174
2Nagyeom <artest1081@gmail.com>
2025-05-27 15:43:42 +0900

    keyboard avoidingView 적용

--- app/Login.tsx
+++ app/Login.tsx
@@ -1,6 +1,6 @@
 import { router } from "expo-router";
 import React from "react";
-import { Dimensions, Image, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Dimensions, Image, Keyboard, KeyboardAvoidingView, Platform, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
 
 
 const { width, height } = Dimensions.get('window')
@@ -13,53 +13,58 @@ export default function Login() {
                 <Image source={require('@/assets/icons/cardLabIcon.png')} style={{ marginVertical: 24, alignSelf: 'center', width: 180, height: 180 }} />
                 <Text style={{ textAlign: 'center', color: '#10375C', fontWeight: '700' }}>A place where you know the market price of all the cards in the world</Text>
             </View>
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.inputBar}
-                    placeholder="ID"
-                    placeholderTextColor={'white'} />
-                <TextInput
-                    style={styles.inputBar}
-                    placeholder="PWD"
-                    placeholderTextColor={'white'}
-                    // text pwd '*' 표시
-                    secureTextEntry={true} />
-                <TouchableOpacity>
-                    <Text style={{ textAlign: 'right', color: '#BFD7EA' }}>Don't you remember the password?</Text>
-                </TouchableOpacity>
-            </View>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                    style={{ flex: 1 }}
+                >
+                    <View style={styles.inputContainer}>
+                        <TextInput
+                            style={styles.inputBar}
+                            placeholder="ID"
+                            placeholderTextColor={'white'} />
+                        <TextInput
+                            style={styles.inputBar}
+                            placeholder="PWD"
+                            placeholderTextColor={'white'}
+                            // text pwd '*' 표시
+                            secureTextEntry={true} />
+                        <TouchableOpacity>
+                            <Text style={{ textAlign: 'right', color: '#BFD7EA' }}>Don't you remember the password?</Text>
+                        </TouchableOpacity>
+                    </View>
+                </KeyboardAvoidingView>
+            </TouchableWithoutFeedback>
             <View style={styles.loginContainer}>
-                <TouchableOpacity 
+                <TouchableOpacity
                     style={styles.loginBtn}
-                    onPress={() => 
-                            (console.log('touch!'))
-                        // router.push('/(tabs)/Home')
-                        
-                    }>
+                    onPress={() => router.push('/(tabs)/Home')}>
                     <Text style={{ textAlign: 'center', color: '#10375C', fontWeight: '600', fontSize: 24 }}>LOGIN</Text>
                 </TouchableOpacity>
-                <View style={styles.otherLoginLine}>
-                    <View style={{ flex: 1, height: 2, backgroundColor: '#1A1A2E' }} />
-                    <Text style={{ textAlign: 'center', color: '#1A1A2E', fontWeight: '500', fontSize: 16 }}>other login</Text>
-                    <View style={{ flex: 1, height: 2, backgroundColor: '#1A1A2E' }} />
-                </View>
-                <View style={styles.otherLoginBtnContainer}>
-                    <TouchableOpacity style={[styles.otherLoginBtn, { backgroundColor: 'black' }]}>
-                        <Image source={require('@/assets/icons/apple_logo.png')} style={{ width: 38, height: 38 }} />
-                        <Text style={{ fontWeight: '600', color: 'white' }}>Go to Apple</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={[styles.otherLoginBtn, { borderWidth: 2, borderColor: '#DADADA' }]}>
-                        <Image source={require('@/assets/icons/google_logo.png')} style={{ width: 34, height: 34 }} />
-                        <Text style={{ fontWeight: '600' }}>Go to Google</Text>
-                    </TouchableOpacity>
+                <View style={{ marginTop: height * 0.1, gap: 12 }}>
+                    <View style={styles.otherLoginLine}>
+                        <View style={{ flex: 1, height: 2, backgroundColor: '#1A1A2E' }} />
+                        <Text style={{ textAlign: 'center', color: '#1A1A2E', fontWeight: '500', fontSize: 16 }}>other login</Text>
+                        <View style={{ flex: 1, height: 2, backgroundColor: '#1A1A2E' }} />
+                    </View>
+                    <View style={styles.otherLoginBtnContainer}>
+                        <TouchableOpacity style={[styles.otherLoginBtn, { backgroundColor: 'black' }]}>
+                            <Image source={require('@/assets/icons/apple_logo.png')} style={{ width: 38, height: 38 }} />
+                            <Text style={{ fontWeight: '600', color: 'white' }}>Go to Apple</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.otherLoginBtn, { borderWidth: 2, borderColor: '#DADADA' }]}>
+                            <Image source={require('@/assets/icons/google_logo.png')} style={{ width: 34, height: 34 }} />
+                            <Text style={{ fontWeight: '600' }}>Go to Google</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
-            <View style={{ marginTop : 'auto', flex : 1, justifyContent : 'center'}}>
+            <View style={{ marginTop: 'auto', height: height * 0.04, justifyContent: 'flex-start' }}>
                 <TouchableOpacity
-                    style={{ alignItems : 'center'}} 
+                    style={{ alignItems: 'center' }}
                     onPress={() => router.push('/Sign')
-                }>
-                    <Text style={{color: '#1A1A2E', fontWeight: '500', fontSize: 14 }}>Are you a new user? Sign up</Text>
+                    }>
+                    <Text style={{ color: '#1A1A2E', fontWeight: '500', fontSize: 14 }}>Are you a new user? Sign up</Text>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
@@ -70,7 +75,8 @@ const styles = StyleSheet.create({
     bgContainer: {
         flex: 1,
         alignItems: 'center',
-        marginHorizontal: 24
+        marginHorizontal: 24,
+
     },
     imgContainer: {
         gap: 18,
@@ -78,22 +84,22 @@ const styles = StyleSheet.create({
         marginHorizontal: 24
     },
     inputContainer: {
-        marginTop: 44,
+        marginTop: 24,
         gap: 12
     },
     inputBar: {
         width: width - 48,
-        height: 34,
+        height: 44,
         paddingHorizontal: 12,
         backgroundColor: '#BFD7EA',
         borderRadius: 8
     },
     loginContainer: {
-        marginTop: 124
+        flex: 1,
     },
     loginBtn: {
         width: width - 48,
-        height: 48,
+        height: 54,
         alignItems: 'center',
         justifyContent: 'center',
         paddingHorizontal: 12,
@@ -106,7 +112,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-between',
         gap: 16,
-        marginVertical: 24,
     },
     otherLoginBtnContainer: {
         width: width - 48,
--- app/Sign.tsx
+++ app/Sign.tsx
@@ -1,23 +1,23 @@
 import { router } from "expo-router";
 import React, { useState } from "react";
-import { Alert, Dimensions, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, Dimensions, Keyboard, KeyboardAvoidingView, Platform, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
 
 const { width, height } = Dimensions.get('window');
 export default function Sign() {
-    const [id, setId] = useState(''); 
-    const [nickName, setNickname] = useState(''); 
-    const [password, setPassword] = useState(''); 
-    const [confirmPassword, setConfirmPassword] = useState(''); 
-    const [email, setEmail] = useState(''); 
+    const [id, setId] = useState('');
+    const [nickName, setNickname] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [email, setEmail] = useState('');
 
 
     const handleSignUp = () => {
-        if(!id || !nickName || !password || !confirmPassword || !email) {
+        if (!id || !nickName || !password || !confirmPassword || !email) {
             Alert.alert('Please fill in all fields!')
             return;
         }
 
-        if(password !== confirmPassword) {
+        if (password !== confirmPassword) {
             Alert.alert('The password doesn\' t match')
             return;
         }
@@ -27,8 +27,8 @@ export default function Sign() {
             '',
             [
                 {
-                    text : 'OK',
-                    onPress : () => router.push('/Login'),
+                    text: 'OK',
+                    onPress: () => router.push('/Login'),
                 },
             ]
         );
@@ -42,97 +42,116 @@ export default function Sign() {
                 <Text style={styles.titleText}>Member Registration</Text>
                 <Text style={styles.titleText}>Welcome!</Text>
             </View>
-            <View style={styles.inputBGConatiner}>
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        placeholder="ID"
-                        placeholderTextColor={'#A2D2FF'}
-                        style={styles.inputTextView}
-                        value={id}
-                        onChangeText={setId}
-                    />
-                    <TextInput
-                        placeholder="NickName"
-                        placeholderTextColor={'#A2D2FF'}
-                        style={styles.inputTextView}
-                        value={nickName}
-                        onChangeText={setNickname}
-                    />
-                    <TextInput
-                        placeholder="Password"
-                        placeholderTextColor={'#A2D2FF'}
-                        style={styles.inputTextView}
-                        value={password}
-                        onChangeText={setPassword}
-                    />
-                    <TextInput
-                        placeholder="Confirm Password"
-                        placeholderTextColor={'#A2D2FF'}
-                        style={styles.inputTextView}
-                        value={confirmPassword}
-                        onChangeText={setConfirmPassword}
-                    />
-                    <TextInput
-                        placeholder="E-mail"
-                        placeholderTextColor={'#A2D2FF'}
-                        style={styles.inputTextView}
-                        value={email}
-                        onChangeText={setEmail}
-                    />
-                </View>
-                <View style={styles.signBtnConatiner}>
-                    <TouchableOpacity 
-                        style={styles.signBtnView}
-                        onPress={handleSignUp}>
-                        <Text style={{ fontWeight: '700', fontSize: 20, color: '#10375C' }}>Sign UP</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity 
-                        style={{ alignItems: 'center' }}
-                        onPress={() => router.push('/Login')}
-                        >
-                        <Text style={{ fontWeight: '600', fontSize: 15, color: '#10375C' }}>Do you remember your ID? Go to Login</Text>
-                    </TouchableOpacity>
-                </View>
+            // input 이 많을 때 KeyboardAvoidingView 를 넣어야함,
+            // KeyboardAvoidingView 가 TouchableWithoutFeedback 보다 부모인 경우 스크롤했을 때
+            // 키보드가 스크롤 도중 포커스를 잃고 사라지는 문제 발생
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                style={{ flex: 1, backgroundColor : '#A2D2FF', borderTopStartRadius : 24, borderTopEndRadius : 24 }}>
+                    <ScrollView
+                        keyboardShouldPersistTaps="handled"
+                        showsVerticalScrollIndicator={false}
+                        bounces={false}
+                    >
+                    <View style={styles.inputBGConatiner}>
+                        <View style={styles.inputContainer}>
+                            <TextInput
+                                placeholder="ID"
+                                placeholderTextColor={'#A2D2FF'}
+                                style={styles.inputBar}
+                                value={id}
+                                onChangeText={setId}
+                            />
+                            <TextInput
+                                placeholder="NickName"
+                                placeholderTextColor={'#A2D2FF'}
+                                style={styles.inputBar}
+                                value={nickName}
+                                onChangeText={setNickname}
+                            />
+                            <TextInput
+                                placeholder="Password"
+                                placeholderTextColor={'#A2D2FF'}
+                                style={styles.inputBar}
+                                value={password}
+                                onChangeText={setPassword}
+                            />
+                            <TextInput
+                                placeholder="Confirm Password"
+                                placeholderTextColor={'#A2D2FF'}
+                                style={styles.inputBar}
+                                value={confirmPassword}
+                                onChangeText={setConfirmPassword}
+                            />
+                            <TextInput
+                                placeholder="E-mail"
+                                placeholderTextColor={'#A2D2FF'}
+                                style={styles.inputBar}
+                                value={email}
+                                onChangeText={setEmail}
+                            />
+                        </View>
+                    </View>
+                    </ScrollView>
+            </KeyboardAvoidingView>
+            </TouchableWithoutFeedback>
+            <View style={styles.signBtnConatiner}>
+                <TouchableOpacity
+                    style={styles.signBtnView}
+                    onPress={handleSignUp}>
+                    <Text style={{ fontWeight: '700', fontSize: 20, color: '#10375C' }}>Sign UP</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={{ alignItems: 'center' }}
+                    onPress={() => router.back()}
+                >
+                    <Text style={{ fontWeight: '600', fontSize: 15, color: '#10375C' }}>Do you remember your ID? Go to Login</Text>
+                </TouchableOpacity>
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    titleText : { 
-        fontWeight: '700', 
-        fontSize: 26, 
-        color: '#10375C' 
+    titleText: {
+        fontWeight: '700',
+        fontSize: 26,
+        color: '#10375C'
     },
-    inputBGConatiner : { 
-        flex: 1, 
-        padding: 24, 
-        backgroundColor: '#A2D2FF', 
-        borderRadius: 24, 
-        alignItems: 'center' 
+    inputBGConatiner: {
+        padding: 24,
+        alignItems: 'center'
     },
-    inputContainer : { 
-        width: '100%', 
-        height: width, 
-        justifyContent: 'space-between', 
-        paddingVertical: 32, 
-        marginBottom : 16 
+    inputContainer: {
+        width: '100%',
+        height: width,
+        justifyContent: 'space-between',
+        paddingVertical: 32,
+        marginBottom: 56,
     },
-    inputTextView : { 
-        paddingLeft: 12, 
-        backgroundColor: '#fff', 
-        height: 44, 
+    inputBar: {
+        paddingLeft: 12,
+        backgroundColor: '#fff',
+        height: 44,
         borderRadius: 8,
     },
-    signBtnConatiner : { 
-        flex: 1, 
-        marginTop: 'auto', 
-        justifyContent: 'center', 
-        width: '100%', 
-        gap: 8 
+    signBtnConatiner: {
+        height: height*0.2,
+        marginTop: 'auto',
+        justifyContent: 'center',
+        width: '100%',
+        paddingHorizontal : 20,
+        backgroundColor : '#A2D2FF',
+        gap: 8,
     },
-    signBtnView : {
-        alignItems: 'center', justifyContent: 'center', borderRadius: 12, backgroundColor: '#fff', height: 50, shadowColor: '#000',
+    signBtnView: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 12,
+        backgroundColor: '#fff',
+        height: 54,
+        shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.35,
         shadowRadius: 3.84,
